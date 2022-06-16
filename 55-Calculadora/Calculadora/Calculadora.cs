10085457e300eefd251997862f31192c61febd6d
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 21:08:11 -0300

    Add files via upload

---
+++ 55-Calculadora/Calculadora/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        public decimal ResultadoSuma (decimal PrimerNumero, decimal SegundoNumero)
+        {
+            return PrimerNumero + SegundoNumero;
+        }
+
+        public decimal ResultadoResta (decimal PrimerNumero, decimal SegundoNumero)
+        {
+            return PrimerNumero - SegundoNumero;
+        }
+        public decimal ResultadoMultiplicacion(decimal PrimerNumero, decimal SegundoNumero)
+        {
+            return PrimerNumero * SegundoNumero;
+        }
+        public decimal ResultadoDivision(decimal PrimerNumero, decimal SegundoNumero)
+        {
+            return PrimerNumero / SegundoNumero;
+        }
+
+        public decimal Calcular(decimal PrimerNumero, decimal SegundoNumero, string operacion)
+        {
+            switch (operacion)
+            {
+                case "sumar":
+                    Console.WriteLine("El resultado de la suma es: " + Calculadora.ResultadoSuma(PrimerNumero, SegundoNumero));
+                    break;
+                case "resta":
+                    Console.WriteLine("El resultado de la resta es: " + Calculadora.ResultadoResta(PrimerNumero, SegundoNumero));
+                    break;
+                case "multiplicacion":
+                    Console.WriteLine("El resultado de la multiplicacion es: " + Calculadora.ResultadoMultiplicacion(PrimerNumero, SegundoNumero));
+                    break;
+                case "division":
+                    Console.WriteLine("El resultado de la division es: " + Calculadora.ResultadoDivision(PrimerNumero, SegundoNumero));
+                    break;
+            }
+        }
+
+    }
+}
---
+++ 55-Calculadora/Calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorLeft = 30;
+
+            Calculadora OperacionesMatematicas = new Calculadora();
+
+            Console.WriteLine("Ingrese 2 numeros:");
+            decimal PrimerNumero = Convert.ToInt32(Console.ReadLine());
+            decimal SegundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escriba la operacion que desea realizar: sumar, resta, multiplicacion o division:");
+            string operacion = Convert.ToString(Console.ReadLine());
+
+            switch (operacion)
+            {
+                case "sumar":
+                    Console.WriteLine("El resultado de la suma es: " + OperacionesMatematicas.ResultadoSuma(PrimerNumero, SegundoNumero));
+                    break;
+                case "resta":
+                    Console.WriteLine("El resultado de la resta es: " + OperacionesMatematicas.ResultadoResta(PrimerNumero, SegundoNumero));
+                    break;
+                case "multiplicacion":
+                    Console.WriteLine("El resultado de la multiplicacion es: " + OperacionesMatematicas.ResultadoMultiplicacion(PrimerNumero, SegundoNumero));
+                    break;
+                case "division":
+                    Console.WriteLine("El resultado de la division es: " + OperacionesMatematicas.ResultadoDivision(PrimerNumero, SegundoNumero));
+                    break;
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
